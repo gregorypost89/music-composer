55e8ca8c88c88e4247f39ded070036d6053a56a5
gregorypost89 <gregorypost89@gmail.com>
2022-05-09 12:14:26 -0400

    Implemented midi writer in main method.  This will create a .midi file in the same directory

--- src/MusicComposer.java
+++ src/MusicComposer.java
@@ -3,10 +3,19 @@ package src;
 import jm.JMC;
 import jm.music.data.*;
 import jm.util.Play;
+import jm.util.Write;
 
 public class MusicComposer implements JMC{
 
     public static void main(String[] args) {
-        Play.midi(new Note(G4,QN));
+        //Writes midi file in the music-composer directory
+        Note note = new Note(C4, DOUBLE_DOTTED_MINIM);
+        Phrase phrase = new Phrase();
+        phrase.addNote(note);
+        Part p = new Part();
+        p.addPhrase(phrase);
+        Score s = new Score("Bing");
+        s.addPart(p);
+        Write.midi(s,"Bing.midi");  //extension can either be .mid or .midi
     }
 }
